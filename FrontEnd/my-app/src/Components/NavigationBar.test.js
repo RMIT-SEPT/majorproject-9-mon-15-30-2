92d8d1b8265615308011aab0f7e1e6ad12b0039e
s3728807 <vdvillaflores@gmail.com>
2020-08-31 20:37:50 +1000

    nav bar unit test

---
+++ FrontEnd/my-app/src/Components/NavigationBar.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {shallow, mount} from "enzyme";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import NavigationBar from "./NavigationBar.js";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<NavigationBar /> Unit Test', () => {
+    it('renders container', () => 
+        {
+            const wrapper = shallow(<NavigationBar />);
+            expect(wrapper.find('.container')).toHaveLength(1);
+        }
+    );
+
+    it('renders 7 nav-item', () =>
+        {
+            const wrapper = shallow(<NavigationBar />);
+            expect(wrapper.find('.nav-item')).toHaveLength(7);
+        }
+    );
+
+
+})
