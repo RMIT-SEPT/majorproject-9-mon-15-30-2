d8f807279ea58a7752dee3e8fc32819f580be1a0
s3728807 <vdvillaflores@gmail.com>
2020-08-31 20:53:02 +1000

    updated new bookings unit test

--- FrontEnd/my-app/src/Components/NewBookings.test.js
+++ FrontEnd/my-app/src/Components/NewBookings.test.js
@@ -6,20 +6,24 @@ import NewBookings from "./NewBookings.js";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<NewBookings /> Unit Test', () => {
+describe('<NewBookings /> Unit Test', () => 
+{
+    it('able to submit form', () =>
+    {
+        const booking = new NewBookings();
+        expect(booking.onSubmit).toHaveLength(1);
+    });
+
     it('renders container', () => 
-        {
-            const wrapper = shallow(<NewBookings />);
-            expect(wrapper.hasClass('container')).toEqual(true);
-        }
-    );
+    {
+        const wrapper = shallow(<NewBookings />);
+        expect(wrapper.hasClass('container')).toEqual(true);
+    });
 
     it('renders 4 form-groups', () =>
-        {
-            const wrapper = shallow(<NewBookings />);
-            expect(wrapper.find('.form-group')).toHaveLength(4);
-        }
-    );
-
+    {
+        const wrapper = shallow(<NewBookings />);
+        expect(wrapper.find('.form-group')).toHaveLength(4);
+    });
 
 })
