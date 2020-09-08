14dee4fe92952aceda667c9d2ce89c0d637b0c6e
s3728807 <vdvillaflores@gmail.com>
2020-09-08 15:23:51 +1000

    finished testing

---
+++ FrontEnd/my-app/src/Components/Test/CurrentBooking.test.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {shallow, mount} from "enzyme";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import CurrentBookings from "../CurrentBookings.js";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<CurrentBooking /> Unit Test', () => 
+{
+    it('renders container for no bookings', () => 
+    {
+        const wrapper = shallow(<CurrentBookings />);
+        expect(wrapper.hasClass('container')).toEqual(true);
+        expect(wrapper.find('.alert')).toHaveLength(1);
+    });
+
+
+    it('renders container', () => 
+    {
+        const work =
+        {
+            fname:"name"
+        };
+        const props = 
+        {
+            id: "a",
+            service: "b",
+            worker: work,
+            date: "d",
+            startTime: "e",
+            endTime: "f"
+        };
+        const current = new CurrentBookings();
+        current.state.currentBookings.push(props);
+        const wrapper = shallow(current.render());
+
+        expect(wrapper.find('.table')).toHaveLength(1);
+        expect(wrapper.find('.th')).toHaveLength(6);
+
+    });
+    
+
+})
