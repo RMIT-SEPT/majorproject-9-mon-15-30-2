5a86b57a783997f8db91b7950b5ce4f8e1ba37e3
Hue Phuong Le s3687477 <s3687477@student.rmit.edu.au>
2020-09-24 06:04:25 +1000

    add missing files

---
+++ BackEnd/src/main/java/com/rmit/sept/monday15302/utils/Response/JWTLoginSucessReponse.java
@@ -0,0 +1,56 @@
+package com.rmit.sept.monday15302.utils.Response;
+
+import com.rmit.sept.monday15302.model.UserType;
+
+public class JWTLoginSucessReponse {
+
+    private boolean success;
+    private String token;
+    private String id;
+    private UserType role;
+
+    public JWTLoginSucessReponse(boolean success, String token, String id, UserType role) {
+        this.success = success;
+        this.token = token;
+        this.id = id;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public UserType getRole() {
+        return role;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public String toString() {
+        return "JWTLoginSucessReponse{" +
+                "success=" + success +
+                ", token='" + token + '\'' +
+                ", id='" + id + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
